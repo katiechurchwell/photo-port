3e236c512b95ac5c5d9f80116ae1c2f5aae99497
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-20 15:42:38 -0700

    about tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    // First Test
+    // Baseline to verify that the component is rendering.
+    it('renders', () => {
+        render(<About />);
+      });
+
+    //Second Test or "test case"
+    it('matches snapshot DOM node structure', () => {
+        //returns a snapshot of the About component
+        const { asFragment } = render(<About />);
+        //test and compare whether the expected and actual outcomes match
+        expect(asFragment()).toMatchSnapshot();
+      });
+  })
